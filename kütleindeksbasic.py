cf22e202669488b8729da4cae06401ca8d015ec0
mucobaN <119523948+mucobanN@users.noreply.github.com>
2022-12-08 22:20:19 +0300

    Update and rename kütleindeksbasic to kütleindeksbasic.py

---
+++ kütleindeksbasic.py
@@ -0,0 +1,4 @@
+
+## girdiler yerine rakam degilde string girersem ne oluyor niye oluyor ? --> lütfen kilonuzu giriniz: gargamel ?
+### Dosya uzantisi yok --> .py vs
+### tek tirnak ve cift tirnak arasinda ne fark var ? --> uzun=input('Uzun Kenar : ') ve boy=int(input("lütfen boyunuz giriniz: ")) arasinda ne fark var ?
