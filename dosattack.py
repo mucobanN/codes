a67d475c5b5a4f0e7aed7ff3c045377a2fa61b21
mucobaN <119523948+mucobanN@users.noreply.github.com>
2022-12-12 23:41:01 +0300

    Create dosattack.py

---
+++ dosattack.py
@@ -0,0 +1,22 @@
+import time
+import os
+
+def get_ip():
+    try:
+        ip=input("lütfen saldırmak istediğiniz ip giriniz: ")
+        ping= int(input("ping boyutu: "))
+        t = int(input("saldırı ne zaman başlasın?(saniye cinsinden): "))
+    except ValueError:
+        print("lütfen doğru bir değer giriniz")
+        get_ip()
+    while t:
+        dak= t//60
+        sny= t%60
+        timer = '{:02d}:{:02d}'.format(dak,sny)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("saldırı başladı")
+    os.system("ping -c {} {}".format(ping, ip))
+    
+get_ip()
